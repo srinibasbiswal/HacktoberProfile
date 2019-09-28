532b7e9018d0a6e2c5e3622c8efb85081bbe64bb
srinibasbiswal <srinibasbiswal12@gmail.com>
2019-09-28 12:19:51 +0530

    Add card placing

--- js/nameMapping.js
+++ js/nameMapping.js
@@ -5,7 +5,7 @@ window.onload = function() {
         name = element.name;
         place = element.place;
         github = element.github;
-        document.getElementById("userCards").innerHTML += "<div class=\"uk-card uk-card-default uk-card-body uk-border-rounded\"> <h2 class=\"name\">"+name+"</h2> <h5 class=\"uk-margin-remove\" class=\"place\">"+place+"</h5> <h3><span uk-icon=\"icon: github\"></span><span class=\"github\">"+github+"</span></h3> </div>"; 
+        document.getElementById("userCards").innerHTML += "<div class=\"uk-width-1-3@m uk-width-1-1@s\"> <div class=\"uk-card uk-card-default uk-card-body uk-border-rounded\"> <h2 class=\"name\">"+name+"</h2> <h5 class=\"uk-margin-remove\" class=\"place\">"+place+"</h5> <h3><span uk-icon=\"icon: github\"></span><span class=\"github\">"+github+"</span></h3> </div> </div>"; 
     });
 };
 
