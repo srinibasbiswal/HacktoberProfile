5d86cd2c8a430bd726482a30f8b2a38637417f4e
Sergi <ocasergi@gmail.com>
2019-10-01 21:14:48 +0200

    Adds my name to the list

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -59,5 +59,10 @@ var profiles = [
         "place" : "Uppsala, Sweden",
         "github" : "Frewacom"
     },
+    {
+        "name" : "Sergi Oca",
+        "place" : "Barcelona, Spain",
+        "github" : "SergiOca87"
+    },
 ]
 
