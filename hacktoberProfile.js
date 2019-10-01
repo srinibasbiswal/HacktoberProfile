20b6c64d2129815b88c487284e3cbcfb498b5efd
Aisha Aqeel <35759625+aishaaqeel@users.noreply.github.com>
2019-10-01 15:43:55 +0500

    Update hacktoberProfile.js

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -10,4 +10,11 @@ var profiles = [
         "place" : "Kuala Lumpur, Malaysia",
         "github" : "csleong98"
     },
+    
+    {
+        "name" : "Aisha Aqeel",
+        "place" : "Karachi, Pakistan",
+        "github" : "aishaaqeel"
+    },
 ]
+
