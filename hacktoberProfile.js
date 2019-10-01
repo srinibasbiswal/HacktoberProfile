5c31b97605dbd955b4d691079ec2b49153100e12
Swaraj Laha (ENU) <swaraj.srj@gmail.com>
2019-10-01 16:25:45 +0530

    Added my profile to hactoberProfile.js

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -1,13 +1,18 @@
-var profiles = [
-    {
-        "name" : "Srinibas Biswal",
-        "place" : "Bhubaneswar, India",
-        "github" : "srinibasbiswal"
+var profiles = [{
+        "name": "Srinibas Biswal",
+        "place": "Bhubaneswar, India",
+        "github": "srinibasbiswal"
     },
-    
+
     {
-        "name" : "Chee Seng Leong",
-        "place" : "Kuala Lumpur, Malaysia",
-        "github" : "csleong98"
+        "name": "Chee Seng Leong",
+        "place": "Kuala Lumpur, Malaysia",
+        "github": "csleong98"
+    },
+
+    {
+        "name": "Swaraj Laha",
+        "place": "Bhubaneswar, India",
+        "github": "swarajlaha"
     },
 ]
