8ba4b1ab50220cc3453cce55b773139754b187d8
gonzalm7 <gonzalm7@wwu.edu>
2019-10-01 11:37:03 -0700

    added my profile

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -1,63 +1,67 @@
 var profiles = [
-    {
-        "name" : "Srinibas Biswal",
-        "place" : "Bhubaneswar, India",
-        "github" : "srinibasbiswal"
-    },    
-    {
-        "name" : "Chee Seng Leong",
-        "place" : "Kuala Lumpur, Malaysia",
-        "github" : "csleong98"
-    },  
-    {
-        "name" : "Aisha Aqeel",
-        "place" : "Karachi, Pakistan",
-        "github" : "aishaaqeel"
-    },
-    {
-        "name" : "Saurabh Shukla",
-        "place" : "Gwalior, India",
-        "github" : "saurabh1515"
-    },
-    {
-        "name" : "runtimeTerror",
-        "place" : "Kharagpur, India",
-        "github" : "hemanth-hk"
-    },
-    {
-        "name" : "Trapta Singh",
-        "place" : "Kanpur, India",
-        "github" : "ts-1"
-    },
-    {
-        "name" : "Juhani AHola",
-        "place" : "Helsinki, Finland",
-        "github" : "aholajuhani"
-    },
-    {
-        "name" : "Himanshu Gupta",
-        "place" : "Noida, India",
-        "github" : "meHimanshu"
-    },
-    {
-        "name" : "Pandiyan Murugan",
-        "place" : "Tamil Nadu, India",
-        "github" : "PandiyanCool"
-    },
-    {
-        "name" : "Rishabh Choudhary",
-        "place" : "Ranchi, India",
-        "github" : "RisavChd24"
-    },
-    {
-        "name" : "Laura Pinto",
-        "place" : "Guimarães, Portugal",
-        "github" : "lauraalpinto"
-    },
-    {
-        "name" : "Fredrik Engstrand",
-        "place" : "Uppsala, Sweden",
-        "github" : "Frewacom"
-    },
-]
-
+  {
+    name: "Srinibas Biswal",
+    place: "Bhubaneswar, India",
+    github: "srinibasbiswal"
+  },
+  {
+    name: "Chee Seng Leong",
+    place: "Kuala Lumpur, Malaysia",
+    github: "csleong98"
+  },
+  {
+    name: "Aisha Aqeel",
+    place: "Karachi, Pakistan",
+    github: "aishaaqeel"
+  },
+  {
+    name: "Saurabh Shukla",
+    place: "Gwalior, India",
+    github: "saurabh1515"
+  },
+  {
+    name: "runtimeTerror",
+    place: "Kharagpur, India",
+    github: "hemanth-hk"
+  },
+  {
+    name: "Trapta Singh",
+    place: "Kanpur, India",
+    github: "ts-1"
+  },
+  {
+    name: "Juhani AHola",
+    place: "Helsinki, Finland",
+    github: "aholajuhani"
+  },
+  {
+    name: "Himanshu Gupta",
+    place: "Noida, India",
+    github: "meHimanshu"
+  },
+  {
+    name: "Pandiyan Murugan",
+    place: "Tamil Nadu, India",
+    github: "PandiyanCool"
+  },
+  {
+    name: "Rishabh Choudhary",
+    place: "Ranchi, India",
+    github: "RisavChd24"
+  },
+  {
+    name: "Laura Pinto",
+    place: "Guimarães, Portugal",
+    github: "lauraalpinto"
+  },
+  {
+    name: "Fredrik Engstrand",
+    place: "Uppsala, Sweden",
+    github: "Frewacom"
+  },
+  {
+    name: "Melissa Gnz",
+    place: "Washington, USA",
+    github: "melisssagnzx"
+  }
+];
