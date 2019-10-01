54fae513b056ede4f3007caca61f49e7b99281fb
DESKTOP-UPQ61CI\geria <geriadam1@gmail.com>
2019-10-01 23:56:28 +0700

    add my profile into list

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -59,5 +59,10 @@ var profiles = [
         "place" : "Uppsala, Sweden",
         "github" : "Frewacom"
     },
+    {
+        "name" : "Geri adam",
+        "place" : "Surabaya, Indonesia",
+        "github" : "geriadam"
+    },
 ]
 
