bb43047d00a9089069a8b939a28cd268b07f5706
Rachel Johnson <rachelktyjohnson@gmail.com>
2019-10-01 22:38:28 +1000

    Update with new profile

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -38,6 +38,11 @@ var profiles = [
         "name" : "Pandiyan Murugan",
         "place" : "Tamil Nadu, India",
         "github" : "PandiyanCool"
+    },
+    {
+        "name" : "Rachel Johnson",
+        "place" : "Sydney, Australia",
+        "github" : "rachelktyjohnson"
     }
 ]
 
