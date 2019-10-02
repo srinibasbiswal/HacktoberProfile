db62b46c3d6a7206a3c9726ddc415f6e138fd84b
lindseywild <lstanek4@gmail.com>
2019-10-01 20:55:56 -0400

    feat: adds lindsey wild

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -59,5 +59,10 @@ var profiles = [
         "place" : "Uppsala, Sweden",
         "github" : "Frewacom"
     },
+    {
+        "name" : "Lindsey Wild",
+        "place" : "Pennsylvania, USA",
+        "github" : "lindseywild"
+    },
 ]
 
