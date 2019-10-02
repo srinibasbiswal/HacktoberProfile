df8f3c8001ffbc03dbf2f4a7650b3c3d254b90e0
Martina Safaric <msafaric@tria.hr>
2019-10-03 00:00:46 +0200

    #hacktoberfest

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -1,252 +1,258 @@
 var profiles = [
-    {
-        "name" : "Srinibas Biswal",
-        "place" : "Bhubaneswar, India",
-        "github" : "srinibasbiswal"
-    },
-    {
-        "name" : "Chee Seng Leong",
-        "place" : "Kuala Lumpur, Malaysia",
-        "github" : "csleong98"
-    },
-    {
-        "name": "Kee Magbanua",
-        "place": "Bacolod, Philippines",
-        "github": "samcode23"
-    },
-    {
-        "name": "Aisha Aqeel",
-        "place": "Karachi, Pakistan",
-        "github": "aishaaqeel"
-    },
-    {
-        "name": "Saurabh Shukla",
-        "place": "Gwalior, India",
-        "github": "saurabh1515"
-    },
-    {
-        "name": "Trapta Singh",
-        "place": "Kanpur, India",
-        "github": "ts-1"
-    },
-    {
-        "name": "Juhani AHola",
-        "place": "Helsinki, Finland",
-        "github": "aholajuhani"
-    },
-    {
-        "name" : "runtimeTerror",
-        "place" : "Kharagpur, India",
-        "github" : "hemanth-hk"
-    },
-    {
-        "name": "Himanshu Gupta",
-        "place": "Noida, India",
-        "github": "meHimanshu"
-    },
-    {
-        "name": "Pandiyan Murugan",
-        "place": "Tamil Nadu, India",
-        "github": "PandiyanCool"
-    },
-    {
-        "name": "HariHaran Subramanian",
-        "place": "Tamil Nadu, India",
-        "github": "gethari"
-    },
-    {
-        "name" : "Mehul Bhutalia",
-        "place" : "Gwalior, India",
-        "github" : "mehul02"
-    },
-    {
-        "name" : "Rachel Johnson",
-        "place" : "Sydney, Australia",
-        "github" : "rachelktyjohnson"
-    },
-    {
-        "name" : "Bolaji Ogidan",
-        "place" : "Nigeria, Lagos",
-        "github" : "Bolajiscott"
-    },
-    {
-        "name" : "Rishabh Choudhary",
-        "place" : "Ranchi, India",
-        "github" : "RisavChd24"
-    },
-    {
-        "name" : "Laura Pinto",
-        "place" : "Guimarães, Portugal",
-        "github" : "lauraalpinto"
-    },
-    {
-        "name": "Alan Saputra Lengkoan",
-        "place": "Makassar, Indonesia",
-        "github": "alanlengkoan"
-    },
-    {
-        "name": "Vikum Sanjeewa",
-        "place": "Kurunegala, Srilanka",
-        "github": "vikumkbv"
-    },
-    {
-        "name" : "Fredrik Engstrand",
-        "place" : "Uppsala, Sweden",
-        "github" : "Frewacom"
-    },
-    {
-        "name" : "William Palacios",
-        "place" : "Managua, Nicaragua",
-        "github" : "wpalacios"
-    },
-    {
-        "name" : "Adrian Mejias",
-        "place" : "Harker Heights, United States",
-        "github" : "adrianmejias"
-    },
-    {
-        "name" : "Eddie Huang",
-        "place" : "San Francisco, USA",
-        "github" : "ehuange"
-    },
-    {
-        "name": "Eric Bryant",
-        "place": "Virginia, USA",
-        "github": "shmickle"
-    },
-    {
-        "name" : "Olajide Joshua",
-        "place" : "Ibadan, Nigeria",
-        "github" : "joshtom"
-    },
-    {
-        "name" : "Olatunji Fatai",
-        "place" : "Ibadan, Nigeria",
-        "github" : "Hollyphat"
-    },
-    {
-        "name" : "Dharmendra kumar",
-        "place" : "kharagpur, India",
-        "github" : "Dharm7900"
-    },
-    {
-        "name" : "Geri adam",
-        "place" : "Surabaya, Indonesia",
-        "github" : "geriadam"
-    },
-    {
-        "name" : "Tharindu Rangika",
-        "place" : "Galle, Sri Lanka",
-        "github" : "TharinduMD"
-    },
-    {
-        "name" : "Joseph Stefani",
-        "place" : "Tacoma, United States",
-        "github" : "josephstefani08"
-    },
-    {
-        "name": "João Vitor Veras",
-        "place": "Franca SP, Brazil",
-        "github": "1develloper"
-    },
-    {
-        "name" : "Joell Lapitan",
-        "place" : "Dubai, UAE",
-        "github" : "jlapitan"
-    },
-    {
-      "name": "Melissa Gnz",
-      "place": "Washington, USA",
-      "github": "melisssagnzx"
-    },
-    {
-        "name" : "Evie Skinner",
-        "place" : "Stroud, UK",
-        "github" : "Evie-Skinner18"
-    },
-    {
+	{
+		"name": "Srinibas Biswal",
+		"place": "Bhubaneswar, India",
+		"github": "srinibasbiswal"
+	},
+	{
+		"name": "Chee Seng Leong",
+		"place": "Kuala Lumpur, Malaysia",
+		"github": "csleong98"
+	},
+	{
+		"name": "Kee Magbanua",
+		"place": "Bacolod, Philippines",
+		"github": "samcode23"
+	},
+	{
+		"name": "Aisha Aqeel",
+		"place": "Karachi, Pakistan",
+		"github": "aishaaqeel"
+	},
+	{
+		"name": "Saurabh Shukla",
+		"place": "Gwalior, India",
+		"github": "saurabh1515"
+	},
+	{
+		"name": "Trapta Singh",
+		"place": "Kanpur, India",
+		"github": "ts-1"
+	},
+	{
+		"name": "Juhani AHola",
+		"place": "Helsinki, Finland",
+		"github": "aholajuhani"
+	},
+	{
+		"name": "runtimeTerror",
+		"place": "Kharagpur, India",
+		"github": "hemanth-hk"
+	},
+	{
+		"name": "Himanshu Gupta",
+		"place": "Noida, India",
+		"github": "meHimanshu"
+	},
+	{
+		"name": "Pandiyan Murugan",
+		"place": "Tamil Nadu, India",
+		"github": "PandiyanCool"
+	},
+	{
+		"name": "HariHaran Subramanian",
+		"place": "Tamil Nadu, India",
+		"github": "gethari"
+	},
+	{
+		"name": "Mehul Bhutalia",
+		"place": "Gwalior, India",
+		"github": "mehul02"
+	},
+	{
+		"name": "Rachel Johnson",
+		"place": "Sydney, Australia",
+		"github": "rachelktyjohnson"
+	},
+	{
+		"name": "Bolaji Ogidan",
+		"place": "Nigeria, Lagos",
+		"github": "Bolajiscott"
+	},
+	{
+		"name": "Rishabh Choudhary",
+		"place": "Ranchi, India",
+		"github": "RisavChd24"
+	},
+	{
+		"name": "Laura Pinto",
+		"place": "Guimarães, Portugal",
+		"github": "lauraalpinto"
+	},
+	{
+		"name": "Alan Saputra Lengkoan",
+		"place": "Makassar, Indonesia",
+		"github": "alanlengkoan"
+	},
+	{
+		"name": "Vikum Sanjeewa",
+		"place": "Kurunegala, Srilanka",
+		"github": "vikumkbv"
+	},
+	{
+		"name": "Fredrik Engstrand",
+		"place": "Uppsala, Sweden",
+		"github": "Frewacom"
+	},
+	{
+		"name": "William Palacios",
+		"place": "Managua, Nicaragua",
+		"github": "wpalacios"
+	},
+	{
+		"name": "Adrian Mejias",
+		"place": "Harker Heights, United States",
+		"github": "adrianmejias"
+	},
+	{
+		"name": "Eddie Huang",
+		"place": "San Francisco, USA",
+		"github": "ehuange"
+	},
+	{
+		"name": "Eric Bryant",
+		"place": "Virginia, USA",
+		"github": "shmickle"
+	},
+	{
+		"name": "Olajide Joshua",
+		"place": "Ibadan, Nigeria",
+		"github": "joshtom"
+	},
+	{
+		"name": "Olatunji Fatai",
+		"place": "Ibadan, Nigeria",
+		"github": "Hollyphat"
+	},
+	{
+		"name": "Dharmendra kumar",
+		"place": "kharagpur, India",
+		"github": "Dharm7900"
+	},
+	{
+		"name": "Geri adam",
+		"place": "Surabaya, Indonesia",
+		"github": "geriadam"
+	},
+	{
+		"name": "Tharindu Rangika",
+		"place": "Galle, Sri Lanka",
+		"github": "TharinduMD"
+	},
+	{
+		"name": "Joseph Stefani",
+		"place": "Tacoma, United States",
+		"github": "josephstefani08"
+	},
+	{
+		"name": "João Vitor Veras",
+		"place": "Franca SP, Brazil",
+		"github": "1develloper"
+	},
+	{
+		"name": "Joell Lapitan",
+		"place": "Dubai, UAE",
+		"github": "jlapitan"
+	},
+	{
+		"name": "Melissa Gnz",
+		"place": "Washington, USA",
+		"github": "melisssagnzx"
+	},
+	{
+		"name": "Evie Skinner",
+		"place": "Stroud, UK",
+		"github": "Evie-Skinner18"
+	},
+	{
 
-        "name" : "Hoang Nguyen",
-        "place" : "Ho Chi Minh, VN",
-        "github" : "phuonghoang713"
-    },
-    {
-        "name" : "Sergi Oca",
-        "place" : "Barcelona, Spain",
-        "github" : "SergiOca87"
-    },
-    {
-        "name" : "Mateo Hoxha",
-        "place" : "Tirane, Albania",
-        "github" : "mateohoxha"
-    },
-    {
-        "name" : "Pelin Uyar",
-        "place" : "Izmir, Turkey",
-        "github" : "p2uyar"
-    },
-    {
-        "name" : "Hatice Erdağı",
-        "place" : "İstanbul, Turkey",
-        "github" : "haticeerdagi"
-    },
-    {
-        "name" : "Calum Dingwall",
-        "place" : "New Jersey, USA",
-        "github" : "CCreativeCND"
-    },
-    {
-        "name" : "Zara Otamias",
-        "place" : "Boston, USA",
-        "github" : "zotamias"
-    },
-    {
-        "name" : "Lindsey Wild",
-        "place" : "Pennsylvania, USA",
-        "github" : "lindseywild"
-    },
-    {
-        "name" : "Felipe Bosi",
-        "place" : "Vila Velha, Brazil",
-        "github" : "bosiarquitetura"
-    },
-    {
-        "name" : "Hari Prasetia",
-        "place" : "Indonesia, Jakarta",
-        "github" : "hariprasetia"
-    },
-    {
-        "name" : "Victor Carli",
-        "place" : "Araraquara, Brazil",
-        "github" : "ocarlic"
-    },
-    {   "name" : "AJEET",
-        "place" : "VARANASI, INDIA",
-        "github" : "ajeet707"
-    },
-    {
-        "name" : "Anna Lewis",
-        "place" : "Denver, United States",
-        "github" : "annnfrann"
-    },
-    {
-        "name" : "Fahad Jamil",
-        "place" : "Uppsala, Sweden",
-        "github" : "McFrappe"
-    },
-    {
-        "name" : "Eduardo Gomes",
-        "place" : "Brasília, Brazil",
-        "github" : "eduqg"
-    },
-    {
-        "name" : "Akinkunmi Olunloye",
-        "place" : "Osogbo, Nigeria",
-        "github" : "Roqak"
-    },
-    {
-        "name" : "Anna Wu",
-        "place" : "New York, USA",
-        "github" : "AnnaYWu"
-    },
+		"name": "Hoang Nguyen",
+		"place": "Ho Chi Minh, VN",
+		"github": "phuonghoang713"
+	},
+	{
+		"name": "Sergi Oca",
+		"place": "Barcelona, Spain",
+		"github": "SergiOca87"
+	},
+	{
+		"name": "Mateo Hoxha",
+		"place": "Tirane, Albania",
+		"github": "mateohoxha"
+	},
+	{
+		"name": "Pelin Uyar",
+		"place": "Izmir, Turkey",
+		"github": "p2uyar"
+	},
+	{
+		"name": "Hatice Erdağı",
+		"place": "İstanbul, Turkey",
+		"github": "haticeerdagi"
+	},
+	{
+		"name": "Calum Dingwall",
+		"place": "New Jersey, USA",
+		"github": "CCreativeCND"
+	},
+	{
+		"name": "Zara Otamias",
+		"place": "Boston, USA",
+		"github": "zotamias"
+	},
+	{
+		"name": "Lindsey Wild",
+		"place": "Pennsylvania, USA",
+		"github": "lindseywild"
+	},
+	{
+		"name": "Felipe Bosi",
+		"place": "Vila Velha, Brazil",
+		"github": "bosiarquitetura"
+	},
+	{
+		"name": "Hari Prasetia",
+		"place": "Indonesia, Jakarta",
+		"github": "hariprasetia"
+	},
+	{
+		"name": "Victor Carli",
+		"place": "Araraquara, Brazil",
+		"github": "ocarlic"
+	},
+	{
+		"name": "AJEET",
+		"place": "VARANASI, INDIA",
+		"github": "ajeet707"
+	},
+	{
+		"name": "Anna Lewis",
+		"place": "Denver, United States",
+		"github": "annnfrann"
+	},
+	{
+		"name": "Fahad Jamil",
+		"place": "Uppsala, Sweden",
+		"github": "McFrappe"
+	},
+	{
+		"name": "Eduardo Gomes",
+		"place": "Brasília, Brazil",
+		"github": "eduqg"
+	},
+	{
+		"name": "Akinkunmi Olunloye",
+		"place": "Osogbo, Nigeria",
+		"github": "Roqak"
+	},
+	{
+		"name": "Anna Wu",
+		"place": "New York, USA",
+		"github": "AnnaYWu"
+	},
+	{
+		"name": "Martina Šafarić",
+		"place": "Čakovec, Croatia",
+		"github": "msafaric"
+	},
 ]
