6e36b9d8d7db50832053d7df73d8f790ef665e68
bosiarquitetura <54243422+bosiarquitetura@users.noreply.github.com>
2019-10-01 22:30:24 -0300

    Added my profile

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -59,5 +59,10 @@ var profiles = [
         "place" : "Uppsala, Sweden",
         "github" : "Frewacom"
     },
+    {
+        "name" : "Felipe Bosi",
+        "place" : "Vila Velha, Brazil",
+        "github" : "bosiarquitetura"
+    },
 ]
 
