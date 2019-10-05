9c09d6020da3ff0bfa9f51fda11cfb7468d30f1b
Thomas Konnemann <kick.butts@gmx.de>
2019-10-05 11:54:58 +0200

    Update hacktoberProfile.js

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -433,6 +433,11 @@ var profiles = [
     name: "Nilesh D",
     place: "Chennai, India",
     github: "Nilesh2000"
+  },
+  {
+    name: "Thomas K.",
+    place: "Münster, Germany",
+    github: "kickbutts"
   }
 ];
 
