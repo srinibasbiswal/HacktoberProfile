6f7b9498ddcec424bcc7a0281d602bd3ff5cf303
dilshan97 <dilshanramesh81@gmail.com>
2019-10-05 23:34:17 +0530

    update hacktoberProfile.js

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -433,6 +433,11 @@ var profiles = [
     name: "Nilesh D",
     place: "Chennai, India",
     github: "Nilesh2000"
+  },
+  {
+    name: "Dilshan Ramesh",
+    place: "Homagama, Sri Lanaka",
+    github: "dilshan97"
   }
 ];
 
