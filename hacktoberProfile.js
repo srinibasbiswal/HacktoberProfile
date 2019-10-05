e0a2721c4b818b248b3f5a8a5e50ed7477a5dd56
Christopher Johnson <CLJohnson088@users.noreply.github.com>
2019-10-05 17:15:56 -0500

    Update hacktoberProfile.js

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -489,5 +489,10 @@ var profiles = [
     place: "Philippines",
     github: "sarahperea"
   
+  },
+  {
+    name: "Christopher Johnson",
+    place: "Florida, USA",
+    github: "CLJohnson088"
   }
 ];
