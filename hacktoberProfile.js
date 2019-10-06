ba67ef1784c4799bb84e1ea012c94741d6fd25d4
Mohd Izzudin Razali <mohdizzudinrazali@gmail.com>
2019-10-06 12:18:45 +0800

    Add Mohd Izzudin as a contributor

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -433,6 +433,11 @@ var profiles = [
     name: "Nilesh D",
     place: "Chennai, India",
     github: "Nilesh2000"
+  },
+  {
+    name: "Mohd Izzudin",
+    place: "Kuala Lumpur, Malaysia",
+    github: "mohdizzudin"
   }
 ];
 
