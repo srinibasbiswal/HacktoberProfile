5691b9f46cf0771a76b38cb8687f5fa910cfd2d9
orichamaru <jvdali966@gmail.com>
2019-10-07 17:14:16 +0530

    Add new user

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -613,5 +613,10 @@ var profiles = [
     name : "Bruna Andrade",
     place : "Salvador, Brasil",
     github : "brunandrade"
+  },
+  {
+    name : "Javed Ali",
+    place: "Allahabad, India",
+    github : "orichamaru"
   }
 ];
