1ecb3ea7228a1795f9bfe853a0192e53169946d8
André Vargas <12630224+andrevargas@users.noreply.github.com>
2019-10-07 09:56:22 -0300

    Update hacktoberProfile.js

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -613,5 +613,10 @@ var profiles = [
     name : "Bruna Andrade",
     place : "Salvador, Brasil",
     github : "brunandrade"
-  }
+  },
+  {
+    name: "André Vargas",
+    place: "Brusque, Brazil",
+    github: "andrevargas",
+  },
 ];
