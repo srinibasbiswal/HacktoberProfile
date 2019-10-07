978f4da09206cd27201b49e07852fec9b9c77e22
Suvojit Barick <53303788+SuvojitBarick@users.noreply.github.com>
2019-10-07 21:08:41 +0530

    Update hacktoberProfile.js

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -613,5 +613,10 @@ var profiles = [
     name : "Bruna Andrade",
     place : "Salvador, Brasil",
     github : "brunandrade"
+  },
+  {
+    name : "Suvojit Barick",
+    place : "Bhubaneswar, India",
+    github : "SuvojitBarick"
   }
 ];
