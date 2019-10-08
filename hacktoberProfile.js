710546d93088ad9e3fa702fe0b3b505151ca0def
ilovesourcecode <53138747+ilovesourcecode@users.noreply.github.com>
2019-10-08 14:46:33 +0200

    Add my details to the profile list

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,10 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+  {
+    name : "Bit Neumann",
+    place : "Cologne, Germany",
+    github : "ilovesourcecode"
   }
 ];
