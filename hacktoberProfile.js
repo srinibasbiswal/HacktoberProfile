7c274f452d622dbfa6b981f0c17bbb48cb3c8738
Juan Manuel <juanma.otegui@gmail.com>
2019-10-08 17:48:30 -0300

    Added

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,10 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+  {
+    name : "Juan Manuel",
+    place : "Montevideo, Uruguay",
+    github : "juanma06"
   }
 ];
