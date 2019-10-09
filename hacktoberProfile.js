30941f9cd73fdea6ea37f83119332d6443dfb57a
Siva Sankar Rajendran <sivasankar-r@outlook.com>
2019-10-09 22:38:23 +0530

    Update hacktoberProfile.js

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,10 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+  {
+    name : "Siva Sankar",
+    place : "Chennai, India",
+    github : "sivasankar-r"
   }
 ];
