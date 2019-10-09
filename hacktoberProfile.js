09d9e674aed89a7fbf402c3cdc196a9aa480ae01
Lena Hartmann <lena.hartmann00@googlemail.com>
2019-10-09 23:37:19 +0200

    added my profile

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,10 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+  {
+    name : "Lena Hartmann",
+    place : "Erding, Germany",
+    github : "lenahartmann00"
   }
 ];
