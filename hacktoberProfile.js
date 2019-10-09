e53b82b016e12b292dfd717188012eecb2506d25
killbotxd <sonawane.1@iitj.ac.in>
2019-10-09 13:11:57 +0530

    add my data

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,10 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+  {
+    name : "Soham Sonawane",
+    place : "Jodhpur, India",
+    github : "killbotXD"
   }
 ];
