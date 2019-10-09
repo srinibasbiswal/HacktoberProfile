d526c27991eaed80e089f6f382edebc43d13ca05
Chanwit Settavongsin <max180643@gmail.com>
2019-10-10 04:24:58 +0700

    add my profile to hacktoberProfile.js

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,10 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+  {
+    name: "max180643",
+    place: "Bangkok, Thailand",
+    github: "max180643"
   }
 ];
