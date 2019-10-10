14037081ffd835aa0d2b5069bcdfce079e1727d1
nikhil karkra <karkra.nikhil@gmail.com>
2019-10-10 13:39:04 +1100

    updating profile

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,10 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+  {
+    name : "Nikhil Karkra",
+    place : "Docklands, Australia",
+    github : "karkranikhil"
   }
 ];
