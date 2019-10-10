39e51581b591d9bf2ca7d1cb1dbd89ee1c4b0707
Sayantan Paul <belikesayantan12@gmail.com>
2019-10-10 15:04:37 +0530

    Added self id for hacktober fest

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,10 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+  {
+    name : "Sayantan Paul",
+    place : "Kolkata, India",
+    github : "belikesayantan"
   }
 ];
