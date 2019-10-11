cc6da64fad76d4f0987a48fb12a17051b296d964
Messias Henrique <40000015+messias-henrique@users.noreply.github.com>
2019-10-10 21:15:25 -0300

    Add Messias Henrique

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,10 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+  {
+   name : "Messias Henrique da Silva Santos",
+   place : "Teresina, Brazil",
+   github : "messias-henrique"
   }
 ];
