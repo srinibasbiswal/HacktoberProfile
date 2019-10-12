68b5709173b30aa2f245b5771b1480a0b0c27b4d
KarthikNannepamula <56467583+KarthikNannepamula@users.noreply.github.com>
2019-10-12 12:21:37 +0530

    Just added my name with the above json

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,11 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+  {
+    name:"Karthik Nannepamula",
+    place: "Erode",
+    github:"KarthikNannepamula",
+   
   }
 ];
