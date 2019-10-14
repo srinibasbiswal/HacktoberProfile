75aa89b6da4ee52ee9d1d14f9f4ad179c61de112
cprobbie <cprobbie23@gmail.com>
2019-10-14 22:35:27 +1100

    added profile

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,10 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+  {
+	  name: "Robbie Cheng",
+	  place: "Melbourne, Australia",
+	  github: "cprobbie"
   }
 ];
