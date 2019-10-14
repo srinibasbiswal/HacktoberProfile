8c6fab1dad0c006c0f0e780fa22a2b01cfffe115
KartikayB <44058155+KartikayB@users.noreply.github.com>
2019-10-15 02:48:36 +0530

    Added my details

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,10 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+  {
+    name : "Kartikay Bansal",
+    place : "Greater Noida, India",
+    github : "KartikayB"
   }
 ];
