e208a2bba9271778fe3ef49b629479d503f3b3c9
Tash925 <tasha.sumo@gmail.com>
2019-10-20 17:33:43 -0400

    Update hacktoberProfile.js
    Adding name to hacktoberProfile.js.

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,10 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+    {
+    name : "Tasha S.",
+    place : "Maryland, United States",
+    github : "Tash925"
   }
 ];
