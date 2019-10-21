b4fe6f0d5d700ad9d52af6beeaf921db83257587
Harsh M <harsh.mehta118@gmail.com>
2019-10-21 16:56:27 +0530

    Add: My git profile info.

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,10 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+  {
+    name : "Harsh M.",
+    place : "Ahmedabad, India",
+    github : "ham118"
   }
 ];
